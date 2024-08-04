15599544efdae3f9d9663cc18caf215cfaef7396
Takayuki Nagatomi <tommyt6073@gmail.com>
2024-08-04 15:38:55 +0900

    Disable auto test generation

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module RailsApiExercises
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators.test_frameworks = false
   end
 end
